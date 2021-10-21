793c09207671761032e63d1803f7c2d71778e8f0
Кузнецов Максим <maximkuznetsov17250@gmail.com>
2021-10-21 22:58:15 +0300

    Add Post.java

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private String name;
+    private int data;
+    private String text;
+    private int likes;
+    private String comments;
+    private int reposts;
+    private int views;
+
+}
